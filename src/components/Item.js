ae868a17aa05ac3c51859da722c6c22731d28861
everythingiswaves <nicolas.g.dellacqua@gmail.com>
2021-10-28 23:57:47 -0300

    desafio clase 6

---
+++ src/components/Item.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Item = ({ id, title, price, pictureUrl }) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Item
---
+++ src/components/ItemList.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import Item from "./Item.js"
+
+const ItemList = ({ items }) => {
+  const [item, setItem] = useState([items]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setItem(items);
+    }, 3000);
+  }, []);
+  if (items.length === 0) {
+    return <p>Loading </p>;
+  } else {
+    return (
+      <div>
+        {items.map((e, i) => {
+          return (
+            <>
+                <Item id={e.id} title={e.title} price={e.price} pictureUrl={e.pictureUrl}/>
+            </>
+          );
+        })}
+      </div>
+    );
+  }
+};
+
+export default ItemList;
