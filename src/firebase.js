d7a07e39d5feba2a57169579911f5d1587485992
everythingiswaves <nicolas.g.dellacqua@gmail.com>
2021-12-08 11:31:33 -0300

    icono y nombre de pag

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,5 @@
-//1) Me traigo firebase
 import firebase from 'firebase/app'
-
-//2) Me traigo firestore
 import 'firebase/firestore'
-
-//3) Copio mis credenciales
 const firebaseConfig = {
   apiKey: "AIzaSyBSvLQ2m-KqtvRpU9gLzOm8AZ-A11SpHX0",
   authDomain: "proyecto-react-coderhous-c7bba.firebaseapp.com",
@@ -13,9 +8,5 @@ const firebaseConfig = {
   messagingSenderId: "245971905836",
   appId: "1:245971905836:web:e1e89b65f797359f819089"
 };
-
-//4) Inicializo firebase
 const app = firebase.initializeApp(firebaseConfig)
-
-//5) Me traigo una referencia de firestore
 export const firestore = firebase.firestore(app) 
